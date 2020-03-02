ab572aeb36aecfef4ea6bf4febd678bd87d048e5
Karina Vieira <karinavic16@gmail.com>
2020-03-02 16:11:12 -0300

    finalização das restriçoẽs, declaração da variavel global

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2 as cv
 import sys, argparse
 import easygui
 
-def ler_video(caminho=None):
+def ler_video(caminho=None):#variavel vazia
     if caminho is None:
         cap = cv.VideoCapture(0) #ler a partir da webcam
     else:
@@ -25,7 +25,7 @@ def ler_video(caminho=None):
 
 def detecta_face(frame, gray):
     face_cascade = 'haarcascade_frontalface_default.xml'
-    faceCascade1 = cv.CascadeClassifier(face_cascade) #classificador para o rosto
+    faceCascade1 = cv.CascadeClassifier(face_cascade) #classificador para a face
     faces = faceCascade1.detectMultiScale(
         gray,
         minNeighbors=20,
@@ -36,9 +36,10 @@ def detecta_face(frame, gray):
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 4)#retangulo da face
-        detecta_olho(roi_gray, roi_color)
+        detecta_olho(roi_gray, roi_color, w)
 
-def detecta_olho(roi_gray, roi_color):
+def detecta_olho(roi_gray, roi_color, w):
+    global ex, ey, pw, ph,ew,eh
     eye_cascade = 'haarcascade_eye.xml'
     faceCascade2 = cv.CascadeClassifier(eye_cascade) #classificador para os olhos
     olhos = faceCascade2.detectMultiScale(
@@ -47,17 +48,16 @@ def detecta_olho(roi_gray, roi_color):
             minSize=(10, 10),
             maxSize=(90,90)
         )
-    cont=1
+    cont=0
     for (ex,ey,ew,eh) in olhos:
-        if cont==1:
+        if cont==0:
             ext=ex
             eyt=ey
             ewt=ew
             eht=eh
             pht = eyt + eht # ponto inferior do olho + altura 
         cont+=1
-    
-    try: #lidando com possiveis erros- sem os dois olhos
+    if cont == 2:
         ph = ey+eh
         if ext < ex:
             pw = ex + ew #ponto superior do olho + largura
@@ -69,13 +69,21 @@ def detecta_olho(roi_gray, roi_color):
             ph = pht
         if eyt < ey:
             ey=eyt
-    except NameError:
-        pass
-    try: #lidando com possiveis erros-nao achar nenhum olho
-            cv.rectangle(roi_color,(ex,ey),(pw,ph), (255,0,0),2)# retangulo dos olhos
+    elif cont == 1:
+        ph = ey+eh
+        ponto_medio = w/2 #ponto medio da largura da face
+        if ex < ponto_medio:  # caso encontre so o olho esquerdo
+            pw = w-ex #coordenada 
+        else:
+            pw = ex+ew
+            l = w-(ex+ew) #largura entre canto direito do olho e a borda da face
+            ex = l
+    try: #lidando com possiveis erros-nao encontrando nenhum olho
+        cv.rectangle(roi_color,(ex,ey),(pw,ph), (255,0,0),2)# retangulo dos olhos
+        cv.rectangle(roi_color,(ex+int(0.5*ew),ey-eh),(pw-int(0.5*ew),ph-int(eh*1.4)), (255,0,255),2)# retangulo da testa
+        roi_testa = roi_color[ey-eh:ph-int(eh*1.4), ex+int(0.5*ew):pw-int(0.5*ew)]
     except:
         pass
-    cv.rectangle(roi_color,(ex+int(0.5*ew),ey-eh),(pw-int(0.5*ew),ph-int(eh*1.4)), (255,0,255),2)# retangulo da testa
 
 def gravar_video(nome):
     cap = cv.VideoCapture(0)
@@ -100,6 +108,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--captura", help="captura o video da webcam", action="store_true")
 args = parser.parse_args()
 if args.captura:
-    gravar_video(inqput("Digite o nome para o video: "))
+    gravar_video(input("Digite o nome para o video: "))
 else:
-    ler_video(easygui.fileopenbox())#chama a funcao ler video
+    ler_video(easygui.fileopenbox())#chama a funcao ler video na pasta do arquivo
+    
+
+'''def RGB_para_HSV(frame): 
+
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV) # Convert BGR em HSV'''
