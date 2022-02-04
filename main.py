9b0208d053350d5da6d5458c056c28ce52b5e678
Karinavic <karinavic16@gmail.com>
2022-02-04 14:51:36 -0300

    inclusao do rmssd

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ import cv2 as cv
 import easygui
 from scipy.signal import butter, filtfilt
 import matplotlib.pyplot as plt
+import heartpy as hp
+from heartpy.datautils import rolling_mean, _sliding_window
+
+
 
 
 def main(caminho=None):  # variavel vazia
@@ -35,7 +39,10 @@ def main(caminho=None):  # variavel vazia
     except NameError:
         print("Erro na leitura, você checou se é um arquivo de vídeo?")
         sys.exit()
-    raw_ppg = np.empty([0])  # armazena os dados brutos de pletismografia
+    n_frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))  # numero total de quadros
+    fps = cap.get(cv.CAP_PROP_FPS)  # numero de quadros por segundo do vídeo
+    raw_ppg = np.zeros([n_frames])  # ppg bruto
+    i_frame = 0  # contador para os quadros
     while True:
         frame = cap.read()[1]  # ler o quadro da imagem do vídeo
         if frame is None:  # fim do vídeo
@@ -46,24 +53,26 @@ def main(caminho=None):  # variavel vazia
         else:
             roi_testa = None
         if roi_testa is not None:  # se encontrou a região da testa
-            media_matiz = calcular_media_matiz(roi_testa)
-            raw_ppg = np.append(raw_ppg, media_matiz)
-        cv.imshow('Video', frame)  # mostra a imagem capturada na janela 
+            raw_ppg[i_frame] = calcular_media_matiz(roi_testa)
+            i_frame += 1
+        cv.imshow('Video', frame)  # mostra a imagem capturada na janela
 
         # o trecho seguinte e apenas para parar o codigo e fechar a janela
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
     cv.destroyAllWindows()
-    calcular_fc(raw_ppg)
+    raw_ppg = raw_ppg[:i_frame]
+    fs_real = len(raw_ppg) / fps
+    calcular_fc(raw_ppg, fs_real)
     print("Done!")
 
 
 def detecta_face(frame):
     """Detecta a face no quadro."""
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    face_cascade_name = 'haarcascade_frontalface_default.xml'
-    face_cascade = cv.CascadeClassifier(face_cascade_name)  # classificador para face
+    face_cascade_name = 'haarcascade_frontalface_default.xml'  # classificador
+    face_cascade = cv.CascadeClassifier(face_cascade_name)     # para a face
     faces = face_cascade.detectMultiScale(gray, minNeighbors=20,
                                           minSize=(30, 30),
                                           maxSize=(300, 300))
@@ -80,8 +89,8 @@ def detecta_face(frame):
 
 def detecta_olho(roi_gray, roi_color):
     """Detecta o olho no quadro."""
-    eye_cascade_name = 'haarcascade_eye.xml'
-    eye_cascade = cv.CascadeClassifier(eye_cascade_name)  # classificador para os olhos
+    eye_cascade_name = 'haarcascade_eye.xml'              # classificador
+    eye_cascade = cv.CascadeClassifier(eye_cascade_name)  # para os olhos
     olhos = eye_cascade.detectMultiScale(roi_gray, minNeighbors=20,
                                          minSize=(10, 10), maxSize=(90, 90))
     contador = 0  # conta a quantidade de olhos encontrados na face
@@ -122,7 +131,7 @@ def detecta_olho(roi_gray, roi_color):
             eye_x = width - (eye_x + eye_wd)
         eyes_wd = width - 2*eye_x
 
-    if contador == 0:  # nenhum olho encontrado no quadro
+    if contador == 0 or len(olhos) > 2:  # nenhum olho encontrado no quadro
         roi_testa = None
     else:
         testa_x = eye_x + int(0.5*eye_wd)
@@ -144,9 +153,9 @@ def gravar_video(nome):
     # Define the codec and create VideoWriter object
     fourcc = cv.VideoWriter_fourcc(*'XVID')
     arquivo = "testevideo_" + nome + ".avi"
-    out = cv.VideoWriter(arquivo, fourcc, 30.0, (640, 480)) #30fps
+    out = cv.VideoWriter(arquivo, fourcc, 30.0, (640, 480))  # 30fps
     contador = 0  # contador do numero de quadros
-    for __ in range(900+150): #900 (duracao do video)+150 (5 segundos)
+    for __ in range(900+150):  # 900 (duracao do video)+150 (5 segundos)
         ret, frame = cap.read()
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
@@ -160,11 +169,11 @@ def gravar_video(nome):
             cv.rectangle(frame, (70-10, 440), (70+10, 480), (0, 255, 0), -1)
         if contador < (180*2) + 150:  # segunda barra
             cv.rectangle(frame, (170-10, 440), (170+10, 480), (0, 255, 0), -1)
-        if contador < (540+150): # terceira barra
+        if contador < (540+150):  # terceira barra
             cv.rectangle(frame, (270-10, 440), (270+10, 480), (0, 255, 0), -1)
-        if contador < (720+150): # quarta barra
+        if contador < (720+150):  # quarta barra
             cv.rectangle(frame, (370-10, 440), (370+10, 480), (0, 255, 0), -1)
-        if contador < (900+150): # quinta barra
+        if contador < (900+150):  # quinta barra
             cv.rectangle(frame, (470-10, 440), (470+10, 480), (0, 255, 0), -1)
         contador += 1
         cv.imshow('frame', frame)
@@ -178,46 +187,55 @@ def gravar_video(nome):
 def calcular_media_matiz(roi_testa):
     """Calcula a média de matiz da região da testa."""
     hsv = cv.cvtColor(roi_testa, cv.COLOR_BGR2HSV)  # Converte BGR em HSV
-    vetor_matiz = np.empty([0])
-    for linha in range(0, hsv.shape[0]):  # percorre linha do frame
-        for coluna in range(0, hsv.shape[1]):  # percorre coluna do frame
-            if hsv[linha, coluna, 0] < 18:  # def. do limite da matiz = 18
-                vetor_matiz = np.append(vetor_matiz, hsv[linha, coluna, 0])
-    media_matiz = np.mean(vetor_matiz)
+    try:
+        media_matiz = np.average(hsv[:, :, 0], weights=(hsv[:, :, 0]) < 18)
+    except ZeroDivisionError:
+        media_matiz = 0
     return media_matiz
 
 
-def calcular_fc(raw_ppg):
+def calcular_fc(raw_ppg, fs):
+    """Calcula a frequência cardíaca a partir do sinal PPG bruto."""
     # calcula frequencia cardiaca- IIR BAND PASS BUTHERWORTH
-    T = 1/20 # periodo
-    fs = 1/T # frequencia de amostragem
-    nyq = 0.5 * fs # Nysquist
-    freq_a = 0.8 / nyq # corte de limite inferior para filtro passa-alto
-    freq_b = 2.2 / nyq # corte de limite superior para filtro passa-baixo
+    T = 1/fs  # periodo de amostragem
+    nyq = 0.5 * fs  # frequência de Nyquist
+    freq_a = 0.8 / nyq  # corte de limite inferior para filtro passa-faixa
+    freq_b = 2.2 / nyq  # corte de limite superior para filtro passa-faixa
 
-    b, a = butter(2, (freq_a, freq_b), btype='bandpass') # filtro de ordem 2
-    ppg_filtrado = filtfilt(b, a, raw_ppg)
+    b, a = butter(2, (freq_a, freq_b), btype='bandpass')  # filtro de ordem 2
+    ppg_filtrado = filtfilt(b, a, raw_ppg)  # obtem o sinal filtrado
 
-    #calculo fft do sinal filtrado
+
+    # calculo fft do sinal filtrado
     N = ppg_filtrado.size
-    t = np.linspace(0, N * T, N) # (base de tempo, valor inicial- final, numero de pontos)
+    t = np.linspace(0, N * T, N)
 
-    fft = np.fft.fft(ppg_filtrado) 
+    fft = np.fft.fft(ppg_filtrado)
 
     # fornece os componentes de frequência correspondentes aos dados
     freq = np.fft.fftfreq(len(ppg_filtrado), T)
     frequencia = freq[:N // 2]
-    amplitude = np.abs(fft)[:N // 2] * 1 / N # normalizando
+    amplitude = np.abs(fft)[:N // 2] * 1 / N  # normalizando
 
     indice_max = np.argmax(amplitude)
     freq_max = frequencia[indice_max]
+
+    #RMSSD 
+    #ppg_filtrado, timer = hp.load_exampledata(0)
+    wd, m = hp.process(ppg_filtrado, sample_rate = fs)
+    wd['peaklist'] #mostra a lista de picos 
+    figura = hp.plotter(wd, m, show=False, title='Picos do sinal FPG')
+    figura.savefig("picos.png", dpi=600)
+    for measure in m.keys():
+        print('%s: %f' %(measure, m[measure]))
+    
 
     plt.figure()
     plt.title("sinal bruto de amplitude no tempo")
     plt.ylabel("Amplitude")
     plt.xlabel("Time [s]")
     plt.plot(t, raw_ppg)
-    plt.savefig('sinal_bruto_no_Tempo.png')
+    plt.savefig('sinal_bruto_no_Tempo.png', dpi=600)
     plt.show()
 
     plt.figure()
@@ -225,7 +243,7 @@ def calcular_fc(raw_ppg):
     plt.ylabel("Amplitude")
     plt.xlabel("Time [s]")
     plt.plot(t, ppg_filtrado)
-    plt.savefig('ppg_filtrado_Tempo.png')
+    plt.savefig('ppg_filtrado_Tempo.png', dpi=600)
     plt.show()
 
     plt.figure()
@@ -233,8 +251,8 @@ def calcular_fc(raw_ppg):
     plt.ylabel("Amplitude")
     plt.xlabel("Frequência (Hz)")
     plt.plot(frequencia, amplitude)
-    print(f" frequencia cardiaca : {freq_max*60}")
-    plt.savefig('ppg_filtrado_fft.png')
+    print(f"Frequência cardíaca: {freq_max*60}")
+    plt.savefig('ppg_filtrado_fft.png', dpi=600)
     plt.show()
 
 
